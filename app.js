9717c8c11d2d38681d698acad80ee142d91fb3e0
evanlewis1 <evanl2@student.unimelb.edu.au>
2018-04-11 16:12:19 +1000

    Attempt to Change site from static to using nodejs properly

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var router = require("routes/routes")
+
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(router);
+
+app.get("styles/style.css", (req, res) => res.send("styles/style.css"))
+app.get("styles/comingsoon.css", (req, res) => res.send("comingsoon/style.css"))
+
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
