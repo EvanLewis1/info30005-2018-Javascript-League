c022909e1f576e3de61e58a486f466e1fc20e71c
Kailun Huang <k.huang20@student.unimelb.edu.au>
2018-04-17 22:13:44 +1000

    update

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ const PORT = process.env.PORT || 3000;
 app.use(router);
 
 app.get("styles/style.css", (req, res) => res.send("styles/style.css"))
+
 app.get("styles/comingsoon.css", (req, res) => res.send("styles/comingsoon.css"))
+
+app.get("styles/where.css", (req, res) => res.send("styles/where.css"))
+
+app.get("styles/Donation.css", (req, res) => res.send("styles/Donation.css"))
+
+app.get("styles/how", (req, res) => res.send("styles/how.css"))
+
+app.get("styles/what.css", (req, res) => res.send("styles/what.css"))
+
 
 
 app.listen(PORT, function(){
