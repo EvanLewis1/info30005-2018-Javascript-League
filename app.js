1ce39d28abe8adebda7a607441044f3dd40de1f9
Kailun Huang <k.huang20@student.unimelb.edu.au>
2018-04-18 03:08:08 +1000

    haha

--- app.js
+++ app.js
@@ -4,20 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-/*
-app.get("styles/style.css", (req, res) => res.send("styles/style.css"))
-
-app.get("styles/comingsoon.css", (req, res) => res.send("styles/comingsoon.css"))
-
-app.get("styles/where.css", (req, res) => res.send("styles/where.css"))
-
-app.get("styles/Donation.css", (req, res) => res.send("styles/Donation.css"))
-
-app.get("styles/how", (req, res) => res.send("styles/how.css"))
-
-app.get("styles/what.css", (req, res) => res.send("styles/what.css"))*/
-
-
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
