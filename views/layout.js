c6a5eb832017ae872c98abe1e3401640f98eb885
evanlewis1 <evanl2@student.unimelb.edu.au>
2018-04-18 15:22:21 +1000

    Created layout.js

---
+++ views/layout.js
@@ -0,0 +1,20 @@
+document.write(`
+
+<div id = includedContent></div>
+
+<div class = "layout menu">
+    <table class = full><tr>
+        <td class = active><a>Why</a></td>
+        <td><a>What</a></td>
+        <td><a>Where</a></td>
+        <td><a>How</a></td>
+        <td><a>Protect Yourself</a></td>
+        <td><a>Pickup Service</a></td>
+        <td><a>Login/Signup</a></td>
+    </tr>
+    </table>
+</div>
+
+
+
+`)
