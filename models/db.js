f02027d3f742374954d6104c54da1f855d5eefe5
Kailun Huang <k.huang20@student.unimelb.edu.au>
2018-05-08 23:34:06 +1000

    1

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://Kailun:82207141@ds261929.mlab.com:61929/recyclingpoint',function(err){
+mongoose.connect('mongodb://user2:1234567@ds261929.mlab.com:61929/recyclingpoint',function(err){
     if(!err){
         console.log("Connect to mongo");
     }else{
